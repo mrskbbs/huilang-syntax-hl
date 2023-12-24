b46ad84feba6a7fddf4fca34751c8e06f2fbdc6c
mrskbbs <sokysokus@gmail.com>
2023-12-24 18:53:13 +0500

    Init repo

---
+++ samples/commands.py
@@ -0,0 +1,66 @@
+COMMANDS = {
+    # python things
+    'from': 'тама',
+    'import': 'спиздить',
+
+    # data types
+    'нумбер': 'int',
+    'плавник': 'float',
+    'стринги': 'str',
+    'малява': 'str',
+    'помойка': 'list',
+    'ксива': 'list',
+    'хуежество': 'set',
+    'букварь': 'dict',
+    'азбука': 'dict',
+
+    # functions
+    'дефине': 'def',
+    'оформить': 'return',
+
+    # conditions
+
+    # exceptions
+    'шухер': 'raise',
+    'нудавайнападай': 'try',
+    'айблядьмаслинупоймал': 'except',
+    'всенахуй': 'finally',
+
+    # lists
+    'равнясь': 'sort',
+    'смирно()': 'sort(reverse=True)',
+    'join': 'вписаться',
+    'map': 'засратьвсе',
+
+    # strings
+    'расчикатилить': 'split',
+    'переобуть': 'replace',
+    'чушпан': 'lower',
+    'авторитет': 'upper',
+    'интеллигент': 'capitalize',
+
+    # generic functions
+    'зырить': 'print',
+    'спросить': 'input',
+
+    # loops
+    'какрулить': 'break',
+    'мишавсехуйня': 'continue',
+
+    # logic operators
+    'внатуре': 'True',
+    'пиздеж': 'False',
+    'однахуйня': 'is',
+    'жиесть': '==',
+    'попизже': '>=',
+    'похуавей': '<=',
+    'пизже': '>',
+    'хуавей': '<',
+
+    # classes
+    'клёво': 'class',
+    'найтистукача': 'assert',
+
+    # punctuation
+    ' бля': ':',
+}
---
+++ samples/test.py
@@ -0,0 +1,39 @@
+nigger = ""
+def func1(nigger, a):
+    s = "Nigga"
+    s.lower()
+    b = s == "nigga"
+    return "jopa"
+func1(nigger, 10.5)
+in
+map
+def jopa(func):
+
+    # python things
+
+    # data types
+
+    # functions
+
+    # conditions
+
+    # exceptions
+
+    # lists
+    равнясь: sort
+    смирно(): sort(reverse=True),
+    join: вписаться
+
+    # strings
+    расчикатилить: split
+    переобуть: replace
+    чушпан: lower
+    авторитет: upper
+    интеллигент: capitalize
+
+    # generic functions
+
+    # logic operators
+    # classes
+
+    # punctuation
